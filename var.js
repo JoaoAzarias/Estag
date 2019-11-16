e6d98ad8926fa57c095ba3a39d02e418326081fc
JoaoAzarias <55952933+JoaoAzarias@users.noreply.github.com>
2019-11-15 22:57:50 -0300

    Add files via upload

---
+++ var.js
@@ -0,0 +1,16 @@
+
+
+mostra = function() {
+        //var minhaVariavel = "Teste";
+        //var nome;
+        //document.getElementById("p_texto").innerHTML = minhaVariavel; 
+        nome=document.getElementById("nome").value ;
+        doc = document.getElementById("doc").value;
+        idade = document.getElementById("idade").value;
+        endereco = document.getElementById("endereco").value;
+        pf=document.getElementById("pf").value;
+        email=document.getElementById("email").value;
+        alert(nome+email+endereco);
+        }
+
+        
